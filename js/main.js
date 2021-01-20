3fa493590a16d31af0876e007d37357261f49469
Ivan Tvisher <65126032+Tvisher@users.noreply.github.com>
2021-01-20 16:54:52 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 
+
 $(function () {
     // прелоадер
     $('.loader_wrapper').delay(1000).fadeOut('slow', function () {
         $('body').removeClass('loading');
     });
-
-
     //вращающийся текс на первом экране
     function $$(selector, context) {
         context = context || document;
@@ -59,6 +58,7 @@ $(function () {
 
         Array.from(document.querySelectorAll('.works__elem picture source')).forEach(elem => {
             let srcset = elem.srcset;
+
             function checkWindowWidth() {
                 if (document.documentElement.clientWidth > 600) {
                     elem.srcset = srcset;
@@ -91,7 +91,7 @@ $(function () {
                 // ball.style.opacity = 0;
                 elem.append(ball);
             }
-        }
+        };
 
         let ballsWrapper = Array.from(document.querySelectorAll('.bals-wrapper'));
         ballsWrapper.forEach(item => {
@@ -132,7 +132,7 @@ $(function () {
             document.body.clientHeight, document.documentElement.clientHeight
         );
         let load = scrollHeight - document.documentElement.clientHeight;
-        let loaded = pageYOffset;
+        let loaded = window.pageYOffset;
         return loaded / load * 100;
     }
     update(loader());
@@ -153,52 +153,8 @@ $(function () {
 });
 
 
-//скрипт формы обратной связи
-window.addEventListener("DOMContentLoaded", function () {
 
-    // get the form elements defined in your form HTML above
 
-    var form = document.getElementById("my-form");
-    var button = document.getElementById("my-form-button");
-    var status = document.getElementById("my-form-status");
-
-    // Success and Error functions for after the form is submitted
-
-    function success() {
-        form.reset();
-        button.style = "display: none ";
-        status.innerHTML = "Thanks!";
-    }
-
-    function error() {
-        status.innerHTML = "Oops! There was a problem.";
-    }
-
-    // handle the form submission event
-
-    form.addEventListener("submit", function (ev) {
-        ev.preventDefault();
-        var data = new FormData(form);
-        ajax(form.method, form.action, data, success, error);
-    });
-});
-
-// helper function for sending an AJAX request
-
-function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState !== XMLHttpRequest.DONE) return;
-        if (xhr.status === 200) {
-            success(xhr.response, xhr.responseType);
-        } else {
-            error(xhr.status, xhr.response, xhr.responseType);
-        }
-    };
-    xhr.send(data);
-}
 
 
 
